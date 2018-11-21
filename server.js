c9c84752f9451b5e1e6181eb6564b290428ffff4
Emanuel Pituch <epituch@purdue.edu>
2018-11-20 19:18:44 -0500

    Created a bunch of pages and added routes for all of them

--- server.js
+++ server.js
@@ -2,10 +2,25 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'mainpage')));
+app.use(express.static(path.join(__dirname, 'loginpage')));
+app.use(express.static(path.join(__dirname, 'leaderboardpage')));
+app.use(express.static(path.join(__dirname, 'createuserpage')));
 let portNumber = 6969;
 
 app.listen(portNumber, () => console.log(`Server started on ${portNumber}`));
 
-app.get('/', (req, res) =>{
-    res.sendFile('mainpage/mainpage.html', {"root": __dirname});
+app.get('/', (req, res) => {
+    res.sendFile('mainpage/mainpage.html', { "root": __dirname });
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile('loginpage/loginpage.html', { "root": __dirname });
+});
+
+app.get('/leaderboard', (req, res) => {
+    res.sendFile('leaderboardpage/leaderboard.html', { "root": __dirname });
+});
+
+app.get('/createuser', (req, res) => {
+    res.sendFile('createuserpage/createuser.html', { "root": __dirname });
 });
