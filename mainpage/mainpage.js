7f5044a389ba73d6e0cac22b779a0738c86a35fa
epituch <epituch@purdue.edu>
2018-12-02 17:00:26 -0500

    Added dragables languages to the play page

--- mainpage/mainpage.js
+++ mainpage/mainpage.js
@@ -1,3 +1,27 @@
+let selectedLanguages = [];
+// TODO: call server for list of languages
+let languageList = {
+    "English": null,
+    "Spanish": null,
+    "Polish": null,
+    "Hindi": null
+};
+
+function addToListOfLanguages(checkInput) {
+    // add to list of languages and clear text
+    let languageSelected = $('input.autocomplete').val();
+    if (languageList[languageSelected] === undefined) {
+        // language does not exist, handle
+        $('#error-div').css('display', 'block');
+    }
+    else {
+        console.log('Added to list of lanaguages');
+        selectedLanguages.push();
+        M.Autocomplete.getInstance($('input.autocomplete')).close();
+        $('input.autocomplete').val('');
+    }
+}
+
 $(document).ready(() => {
     $('select').formSelect();
     $('.sidenav').sidenav();
@@ -5,4 +29,22 @@ $(document).ready(() => {
     $('#garble-text-btn').click(() => {
         $('#results').css('visibility', 'visible');
     });
+
+    $('input.autocomplete').autocomplete({
+        data: languageList,
+        onAutocomplete: () => {
+            addToListOfLanguages();
+        }
+    });
+    $('input.autocomplete').on('keyup', (event) => {
+        // listens for enter event inside the field
+        if (event.key === 'Enter') {
+            // key is enter add to list after verificiation
+            addToListOfLanguages();
+        }
+        else {
+            $('#error-div').css('display', 'none');
+        }
+    });
+    $('#languages').sortable();
 });
