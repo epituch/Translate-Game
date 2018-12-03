4e98bfa7e08a804dc07f7892029926c6f61b5cec
epituch <epituch@purdue.edu>
2018-12-03 16:23:22 -0500

    Added error so user cannot select more than 10 languages

--- mainpage/mainpage.js
+++ mainpage/mainpage.js
@@ -1,4 +1,5 @@
 let currId = 0;
+let numLanguages = 0;
 
 // TODO: call server for list of languages
 let languageList = {
@@ -12,16 +13,23 @@ let languageList = {
 *   Checks the text that in the box and adds it to html
 *   and the array if it is valid
 */
-function addToListOfLanguages(checkInput) {
+function addToListOfLanguages() {
     // add to list of languages and clear text
     let languageSelected = $('input.autocomplete').val();
     if (languageList[languageSelected] === undefined) {
         // language does not exist, handle
+        $('#error-div').html('Error: Language does not exist');
+        $('#error-div').css('display', 'block');
+        return;
+    }
+    else if(numLanguages > 10){
+        $('#error-div').html('Error: You can only add up to 10 languages');
         $('#error-div').css('display', 'block');
     }
     else {
         addLanguageBox(languageSelected);
         currId++;
+        numLanguages++;
         M.Autocomplete.getInstance($('input.autocomplete')).close();
         $('input.autocomplete').val('');
     }
@@ -41,6 +49,7 @@ function addLanguageBox(language) {
 */
 function remove(id) {
     $('#' + id).remove();
+    numLanguages--;
 }
 
 /*
